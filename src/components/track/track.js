21ac4b133d1ebc4115d97719e90facbedfb3c6a4
flydecisive <flydecisive@yandex.ru>
2023-08-22 17:06:22 +0300

    исправил отображение треков

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -34,19 +34,21 @@ export const StyledTime = styled.div`
 `;
 
 export const StyledTitleImage = styled.div`
-  width: 25px;
-  height: 25px;
+  width: 35px;
+  height: 35px;
   padding: 16px;
   background: ${(props) => (props.theme.theme ? '#F6F4F4' : '#313131')};
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
 
 export const StyledTitleSvg = styled.svg`
-  width: 25px;
-  height: 25px;
+  width: 20px;
+  height: 20px;
   fill: transparent;
+  position: absolute;
   stroke: ${(props) => (props.theme.theme ? '#B1B1B1' : '#4e4e4e')};
 `;
 
